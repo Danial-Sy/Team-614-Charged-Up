4955b4cdf0ec73d6ff0f8a06d38d9136dcab28a9
zabzzz <121322821+zabzzz@users.noreply.github.com>
2023-04-02 15:34:01 -0400

    Event Map for "Sling3GP"
    Event map for the auto which scores 3 game pieces on the blue side

--- AdvancedCommandRobotExample/src/main/java/frc/robot/commands/PathPlannerCommands/EventMap.java
+++ AdvancedCommandRobotExample/src/main/java/frc/robot/commands/PathPlannerCommands/EventMap.java
@@ -5,7 +5,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.commands.PIDCommand.TiltPIDCommand;
 import frc.robot.commands.SequentialParallelCommands.GroundIntake;
+import frc.robot.commands.SequentialParallelCommands.ScoreHybrid;
 import frc.robot.commands.SequentialParallelCommands.ScoreMidCone;
+import frc.robot.commands.SequentialParallelCommands.ScoreMidCubeAuto;
 
 public class EventMap {
 
@@ -24,5 +26,17 @@ public class EventMap {
     HashMap<String, Command> ScoreBalance = new HashMap<>();
     ScoreBalance.put("balance", new AutoBalance());
     return ScoreBalance;
+  }
+
+  public static HashMap<String, Command> Sling3GP() {
+    HashMap<String, Command> Sling3GP = new HashMap<>();
+    Sling3GP.put("stow", new TiltPIDCommand(Constants.TILT_DEFAULT_SETPOINT));
+    Sling3GP.put("intake", new GroundIntake());
+    Sling3GP.put("armup", new TiltPIDCommand(Constants.TILT_DEFAULT_SETPOINT));
+    Sling3GP.put("scoremid", new ScoreMidCubeAuto());
+    Sling3GP.put("intake1", new GroundIntake());
+    Sling3GP.put("armup1", new TiltPIDCommand(Constants.TILT_DEFAULT_SETPOINT));
+    Sling3GP.put("scorelow", new ScoreHybrid());
+    return Sling3GP();
   }
 }
